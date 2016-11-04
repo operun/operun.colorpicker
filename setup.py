7a0bbba42d29d089a6fc76dba7416b755dfb93e4
Jesse Stippel <jesse.stippel@operun.de>
2016-11-04 11:04:41 +0100

    updated version to 1.0.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='operun.colorpicker',
-    version='1.0a1',
+    version='1.0.1',
     description="A Plone Bootstrap color picker package.",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
